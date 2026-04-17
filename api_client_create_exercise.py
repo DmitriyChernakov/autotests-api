8c9bef3e1ebcf84b3f1506f96f3a0a6163b848e7
Dmitriy <ChernakovDMT@yandex.ru>
2026-04-17 12:38:00 +0700

    Применяем Pydantic в API клиентах

--- api_client_create_exercise.py
+++ api_client_create_exercise.py
@@ -1,32 +1,34 @@
 from clients.courses.courses_client import get_courses_client, CreateCourseRequestDict
 from clients.exercises.exercises_client import get_exercise_client, CreateExerciseRequestDict
-from clients.files.files_client import get_files_client, CreateFileRequestDict
-from clients.private_http_builder import AuthenticationUserDict
+from clients.files.files_client import get_files_client
+from clients.private_http_builder import AuthenticationUserSchema
 from clients.users.private_users_client import get_private_users_client
-from clients.users.public_users_client import get_public_users_client, CreateUserRequestDict
+from clients.users.public_users_client import get_public_users_client
+from clients.users.users_schema import CreateUserRequestSchema
+from clients.files.files_schema import CreateFileRequestSchema
 from tools.fakers import get_random_email
 
 public_users_client = get_public_users_client()
 
-create_user_request = CreateUserRequestDict(
+create_user_request = CreateUserRequestSchema(
     email=get_random_email(),
     password='string',
-    lastName='string',
-    firstName='string',
-    middleName='string'
+    last_name='string',
+    first_name='string',
+    middle_name='string'
 )
 create_user_response = public_users_client.create_user(create_user_request)
 
-authentication_user = AuthenticationUserDict(
-    email=create_user_request['email'],
-    password=create_user_request['password']
+authentication_user = AuthenticationUserSchema(
+    email=create_user_request.email,
+    password=create_user_request.password
 )
 private_users_client = get_private_users_client(authentication_user)
 files_client = get_files_client(authentication_user)
 course_client = get_courses_client(authentication_user)
 exercise_client = get_exercise_client(authentication_user)
 
-create_file_request = CreateFileRequestDict(
+create_file_request = CreateFileRequestSchema(
     filename='image.png',
     directory='courses',
     upload_file='./testdata/files/image.png',
@@ -40,8 +42,8 @@ create_course_request = CreateCourseRequestDict(
     minScore=10,
     description="Python API course",
     estimatedTime="2 weeks",
-    previewFileId=create_file_response['file']['id'],
-    createdByUserId=create_user_response['user']['id']
+    previewFileId=create_file_response.file.id,
+    createdByUserId=create_user_response.user.id
 )
 create_course_response = course_client.create_course(create_course_request)
 print("Create course data: ", create_course_response)
