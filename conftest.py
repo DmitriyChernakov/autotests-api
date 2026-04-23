a2591b4eed6adfd879254b205d006832dcbba104
Dmitriy <ChernakovDMT@yandex.ru>
2026-04-23 15:40:26 +0700

    Рефакторинг и подготовка к написанию API автотестов. Практика реализации фикстур

--- conftest.py
+++ conftest.py
@@ -2,5 +2,6 @@ pytest_plugins = (
     "fixtures.users",
     "fixtures.files",
     "fixtures.courses",
+    "fixture.exercises",
     "fixtures.authentication"
 )
---
+++ fixtures/exercises.py
@@ -0,0 +1,29 @@
+import pytest
+from pydantic import BaseModel
+
+from clients.exercises.exercises_client import ExercisesClient, get_exercise_client
+from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema
+from fixtures.courses import CourseFixture
+from fixtures.users import UserFixture
+
+
+class ExerciseFixture(BaseModel):
+    """
+    Модель для объединения возвращаемых данных фикстурой function_exercise
+    """
+    request: CreateExerciseRequestSchema
+    response: CreateExerciseResponseSchema
+
+
+@pytest.fixture
+def exercises_client(function_user: UserFixture) -> ExercisesClient:
+    return get_exercise_client(function_user.authentication_user)
+
+
+@pytest.fixture
+def function_exercise(exercises_client: ExercisesClient, function_course: CourseFixture) -> ExerciseFixture:
+    request = CreateExerciseRequestSchema(
+        course_id=function_course.response.course.id
+    )
+    response = exercises_client.create_exercise(request)
+    return ExerciseFixture(request=request, response=response)
