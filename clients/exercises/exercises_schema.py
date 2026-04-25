6ed430f0da412a853220bf97017bcf18f932de44
Dmitriy <ChernakovDMT@yandex.ru>
2026-04-25 23:38:49 +0700

    Практикуемся в написании API автотестов. Часть 3. Практика написания API автотестов для работы с заданиями. Обновление задания

--- clients/exercises/exercises_schema.py
+++ clients/exercises/exercises_schema.py
@@ -68,12 +68,12 @@ class UpdateExerciseRequestSchema(BaseModel):
     """
     model_config = ConfigDict(validate_by_name=True, validate_by_alias=True)
 
-    title: str = Field(default_factory=fake.sentence)
-    max_score: int = Field(alias="maxScore", default_factory=fake.max_score)
-    min_score: int = Field(alias="minScore", default_factory=fake.min_score)
-    order_index: int = Field(alias="orderIndex", default_factory=fake.integer)
-    description: str = Field(default_factory=fake.text)
-    estimated_time: str = Field(alias="estimatedTime", default_factory=fake.estimated_time)
+    title: str | None = Field(default_factory=fake.sentence)
+    max_score: int | None = Field(alias="maxScore", default_factory=fake.max_score)
+    min_score: int | None = Field(alias="minScore", default_factory=fake.min_score)
+    order_index: int | None = Field(alias="orderIndex", default_factory=fake.integer)
+    description: str | None = Field(default_factory=fake.text)
+    estimated_time: str | None = Field(alias="estimatedTime", default_factory=fake.estimated_time)
 
 
 class UpdateExerciseResponseSchema(BaseModel):
--- tools/assertions/exercises.py
+++ tools/assertions/exercises.py
@@ -2,7 +2,9 @@ from clients.exercises.exercises_schema import (
     CreateExerciseRequestSchema,
     CreateExerciseResponseSchema,
     ExerciseSchema,
-    GetExerciseResponseSchema
+    GetExerciseResponseSchema,
+    UpdateExerciseRequestSchema,
+    UpdateExerciseResponseSchema
 )
 from tools.assertions.base import assert_equal
 
@@ -57,3 +59,22 @@ def assert_get_exercise_response(
     :raises AssertionError: Если данные упражнения не совпадают.
     """
     assert_exercise(actual=get_exercise_response.exercise, expected=create_exercise_response.exercise)
+
+
+def assert_update_exercise_response(
+        request: UpdateExerciseRequestSchema,
+        response: UpdateExerciseResponseSchema
+):
+    """
+    Проверяет, что ответ на обновление упражнения соответствует запросу.
+
+    :param request: Исходный запрос на обновление упражнения.
+    :param response: Ответ API с обновленными данными упражнения.
+    :raises AssertionError: Если хотя бы одно поле не совпадает.
+    """
+    assert_equal(response.exercise.title, request.title, "title")
+    assert_equal(response.exercise.max_score, request.max_score, "max_score")
+    assert_equal(response.exercise.min_score, request.min_score, "min_score")
+    assert_equal(response.exercise.order_index, request.order_index, "order_index")
+    assert_equal(response.exercise.description, request.description, "description")
+    assert_equal(response.exercise.estimated_time, request.estimated_time, "estimated_time")
