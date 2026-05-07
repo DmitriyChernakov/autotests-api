5c2ba48f098b9d9d53cf6b54ce621970123a47cc
Dmitriy <ChernakovDMT@yandex.ru>
2026-05-07 15:06:44 +0700

    Улучшение работы с API клиентами с помощью Enum для URI. Практика работы с использованием APIRoutes в API-клиентах

--- clients/exercises/exercises_client.py
+++ clients/exercises/exercises_client.py
@@ -12,6 +12,7 @@ from clients.exercises.exercises_schema import (
     UpdateExerciseResponseSchema
 )
 from clients.private_http_builder import AuthenticationUserSchema, get_private_http_client
+from tools.routes import APIRoutes
 
 
 class ExercisesClient(APIClient):
@@ -28,7 +29,7 @@ class ExercisesClient(APIClient):
         :return: Ответ от сервера в виде объекта httpx.Response
         """
         return self.get(
-            "/api/v1/exercises",
+            APIRoutes.EXERCISES,
             params=query.model_dump(by_alias=True)
         )
 
@@ -40,7 +41,7 @@ class ExercisesClient(APIClient):
         :param exercise_id: Идентификатор упражнения.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.get(f"/api/v1/exercises/{exercise_id}")
+        return self.get(f"{APIRoutes.EXERCISES}/{exercise_id}")
 
     @allure.step("Create exercise")
     def create_exercise_api(self, request: CreateExerciseRequestSchema) -> Response:
@@ -51,7 +52,7 @@ class ExercisesClient(APIClient):
         :return: Ответ от сервера в виде объекта httpx.Response
         """
         return self.post(
-            "/api/v1/exercises",
+            APIRoutes.EXERCISES,
             json=request.model_dump(by_alias=True)
         )
 
@@ -65,7 +66,7 @@ class ExercisesClient(APIClient):
         :return: Ответ от сервера в виде объекта httpx.Response
         """
         return self.patch(
-            f"/api/v1/exercises/{exercise_id}",
+            f"{APIRoutes.EXERCISES}/{exercise_id}",
             json=request.model_dump(by_alias=True)
         )
 
@@ -77,7 +78,7 @@ class ExercisesClient(APIClient):
         :param exercise_id: Идентификатор курса.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.delete(f"/api/v1/exercises/{exercise_id}")
+        return self.delete(f"{APIRoutes.EXERCISES}/{exercise_id}")
 
     def get_exercises(self, query: GetExercisesQuerySchema) -> GetExercisesResponseSchema:
         """
